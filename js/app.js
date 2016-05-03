56e7fb3f79e80a4645d712eb9b3d0465df35f8db
Hewan Abreha <henebys@gmail.com>
2016-05-03 09:03:24 -0400

    updated, added, files

--- js/app.js
+++ js/app.js
@@ -1,5 +1,36 @@
 "use strict";
 
 (function(){
-  angular.module("wdinstagram", [])
+
+  angular
+  .module("wdinstagram", ["ui.router"])
+  .config(["$stateProvider","$locationProvider", RouterFunction])
+  .controller("PostIndexController", PostIndexControllerFunc)
+  .controller("PostShowController", PostShowControllerFunc)
+  .factory("PostFactory", PostFactoryFunc);
+
+  .config([
+    "$stateProvider",
+    "$locationProvider",
+    RouterFunction
+  ]);
+
+
+  function RouterFuction($stateProvider) {
+    $stateProvider
+    .state("postIndex", {
+      url: "/posts",
+      templateUrl: "js/index.html",
+      controller: "PostIndexController",
+      controllerAs: "indexVm"
+    })
+    .state("PostsShow", {
+      url: "/posts/:id",
+      templateUrl: "js/show.html",
+      controller: "PostShowController",
+      controllerAs: "posyshowVm"
+    });
+  }
+
+}
 })();
